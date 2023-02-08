6f774de5390045f227d279da3aaee522d7e245ff
Erik Hanson <erik@erikhanson.dev>
2023-02-08 15:38:54 -0800

    Update copyright statements

--- GopherThemePlugin.inc.php
+++ GopherThemePlugin.inc.php
@@ -5,6 +5,7 @@
  *
  * Copyright (c) 2014-2023 Simon Fraser University
  * Copyright (c) 2003-2023 John Willinsky
+ * Copyright (c) 2023 University of Minnesota
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @class GopherThemePlugin
