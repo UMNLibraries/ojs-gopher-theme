9a27f7a624da15c5132c42341f41bf169e05b34e
Erik Hanson <erik@erikhanson.dev>
2023-02-14 09:12:59 -0800

    Update license notice to GPL v3 where outdated

--- GopherThemePlugin.inc.php
+++ GopherThemePlugin.inc.php
@@ -5,7 +5,7 @@
  *
  * Copyright (c) 2023 Simon Fraser University
  * Copyright (c) 2023 University of Minnesota
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class GopherThemePlugin
  * @ingroup plugins_themes_gopher
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  *
  * Copyright (c) 2014-2022 Simon Fraser University
  * Copyright (c) 2003-2022 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @ingroup plugins_themes_gopher
  * @brief Wrapper for Gopher theme plugin.
